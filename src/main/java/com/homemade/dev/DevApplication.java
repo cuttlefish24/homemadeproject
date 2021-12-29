690cc664613095744215705a2885ebce697dc16f
cuttlefish24 <havoc_06_sibling@icloud.com>
2021-12-29 15:48:09 +0900

    20211229_first commit

---
+++ src/main/java/com/homemade/dev/DevApplication.java
@@ -0,0 +1,25 @@
+package com.homemade.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		String response = String.format("Hello %s", name); 
+
+		System.out.println("[hello] response_data : [" + response + "]");
+
+		return response;
+	}
+
+}
